7efb457efe253f2473484d20309abe95aa5cc2e7
Nickss09 <nicoliemuller0iap@gmail.com>
2025-05-02 12:16:41 -0300

    Vetores e Ação

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,51 @@
+import { useState } from 'react'
 import './App.css'
 import Post from './components/Post'
 
-function App() {
+const postagens = [
+  {
+    usuario: 'Nicolie Müller',
+    descricao: 'Sol nasceu, dourando o riso, brinca o dia no improviso. Passa o tempo sem medida, leve, feito flor nascida. Canta o mundo em tom sincero, tudo é belo, tudo é vero.',
+    link: 'https://static.vecteezy.com/system/resources/thumbnails/019/879/186/small/user-icon-on-transparent-background-free-png.png',
+    datahora: '25/04/2025 11:40'
+  },
+  {
+    usuario: 'Isadora Abreu',
+    descricao: 'No silêncio da noite serena, Sussurra o vento em voz pequena. A lua observa, calma e cheia, Enquanto o tempo se despeneia.',
+    link: 'https://static.vecteezy.com/system/resources/thumbnails/019/879/186/small/user-icon-on-transparent-background-free-png.png',
+    datahora: '02/05/2025 11:30'
+  }
+]
 
+const colors=['red','white', 'green', 'yellow']
+
+function App() {
+  const [contador, setContador] = useState(0)
+
+//const [variavelParaValor, funçãoMudaValor]
+// = useState(Valor)
   return (
     <>
-    <div className="header">
-      <h1>Blog</h1>
-    </div>
-      <Post 
-        usuario='Nicolie Müller'
-        descricao='"Sol nasceu, dourando o riso,
-                    brinca o dia no improviso.
-                    Passa o tempo sem medida,
-                    leve, feito flor nascida.
-                    Canta o mundo em tom sincero,
-                    tudo é belo, tudo é vero."'
-        link='https://static.vecteezy.com/system/resources/thumbnails/019/879/186/small/user-icon-on-transparent-background-free-png.png'
-        datahora='25/04/2025 11:40'
+      <div className="header">
+        <h1>Blog</h1>
+      </div>
+      <Post usuario={postagens[0].usuario}
+        descricao={postagens[0].descricao}
+        link={postagens[0].link}
+        datahora={postagens[0].datahora}
+        cor='pink'
       />
+      <Post usuario={postagens[1].usuario}
+        descricao={postagens[1].descricao}
+        link={postagens[1].link}
+        datahora={postagens[1].datahora}
+        cor='green'
+      />
+      <div>
+        <p>Total de posts: {contador}</p>
+        <button onClick={()=>setContador(contador+1)}>Aumentar Posts</button>
+        <button onClick={()=>setContador(contador-1)}>Diminuir Posts</button>
+      </div>
     </>
   )
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import './Post.css'
 
-function Post({usuario, descricao, link, datahora}){
-    return (
-        <>
-            <div className='caixa'>
-                <div className='perfil'>
-                  <img src={link} className="foto" />
-                  <h2 className='nome'>{usuario}</h2>
-                </div>
-                  <p className='descricao'>{descricao}</p>
-                <div className='data-hora'>{datahora}</div>
-            </div>
-        </>
-        )
+const Post = ({ usuario, descricao, link, datahora, cor}) => {
+  return (
+    <>
+      <div className='caixa' style={{background:cor}}>
+        <div className='perfil'>
+          <img src={link} className="foto" />
+          <h2 className='nome'>{usuario}</h2>
+        </div>
+        <p className='descricao'>{descricao}</p>
+        <div className='data-hora'>{datahora}</div>
+      </div>
+    </>
+  )
 }
 export default Post
